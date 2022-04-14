dfd8d31977d9d95fbf48db689ccb85d072f00819
Utkarsh <utripathi2002@gmail.com>
2022-04-14 11:17:56 +0530

    attempting to add google tag manager

--- src/App.js
+++ src/App.js
@@ -10,16 +10,7 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import ReactDOM from "react-dom";
 import Footer from "./components/Footer";
-import TagManager from "react-gtm-module";
-
-const tagManagerArgs = {
-  gtmId: "G-Y05P49V84Y",
-};
-TagManager.initialize(tagManagerArgs);
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  console.log(loading);
   const [showArrow, setShowArrow] = useState(true);
   useEffect(() => {
     if (window.innerWidth > 768) {
@@ -36,50 +27,40 @@ function App() {
     });
   }, []);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <>
-        <section id="landing" className={styles.landing_container}>
-          <Navbar />
-          <Landing />
-          {showArrow && (
-            <a href="#about">
-              <ArrowDown className={styles.arrow} size={48} color="#6666ff" />
-            </a>
-          )}
-        </section>
-        <section className={styles.about_me} id="about">
-          <About />
-        </section>
-        <section className={styles.project} id="project">
-          <Projects change={showArrow} />
-        </section>
-        <section id="contact" className="mb-3">
-          <Contact />
-        </section>
-        {ReactDOM.createPortal(
-          <Footer />,
-          document.getElementById("footer-root")
+  return (
+    <>
+      <section id="landing" className={styles.landing_container}>
+        <Navbar />
+        <Landing />
+        {showArrow && (
+          <a href="#about">
+            <ArrowDown className={styles.arrow} size={48} color="#6666ff" />
+          </a>
         )}
-        {ReactDOM.createPortal(
-          <a href="#landing">
-            <div className={styles.toTop}>
-              <ArrowUp size={32} color="#fff" weight="fill" />
-            </div>
-          </a>,
-          document.getElementById("back-to-top")
-        )}
-      </>
-    );
-  }
+      </section>
+      <section className={styles.about_me} id="about">
+        <About />
+      </section>
+      <section className={styles.project} id="project">
+        <Projects change={showArrow} />
+      </section>
+      <section id="contact" className="mb-3">
+        <Contact />
+      </section>
+      {ReactDOM.createPortal(
+        <Footer />,
+        document.getElementById("footer-root")
+      )}
+      {ReactDOM.createPortal(
+        <a href="#landing">
+          <div className={styles.toTop}>
+            <ArrowUp size={32} color="#fff" weight="fill" />
+          </div>
+        </a>,
+        document.getElementById("back-to-top")
+      )}
+    </>
+  );
 }
 
 export default App;
